d82f92b3878db084a9046a5a475c34468f2364df
jl98swe <jl1998swe@gmail.com>
2024-06-27 11:46:23 +0200

    Lagt till felhantering av vektor/sträng.

--- plocka_forsta_element_func.R
+++ plocka_forsta_element_func.R
@@ -1,11 +1,18 @@
 # Funktion som returnerar det första elementet från en vektor. Kan även ta bort elementet från vektorn i den globala miljön.
 plocka_forsta_element <- function(vektor, taBort = TRUE) {
   
-  # Hämta namnet på vektorn som en sträng.
-  vektorNamn <- deparse(substitute(vektor))
-  
-  # Hämta vektorn från det globala miljön.
-  vektor <- get(vektorNamn, envir = .GlobalEnv)
+  # Om en vektor matats in under argumentet 'vektor' så måste vektorns namn sparas.
+  if (!(is.character(vektor))) {
+    # Hämta namnet på vektorn som en sträng.
+    vektorNamn <- deparse(substitute(vektor))
+  }
+  else { # Om istället en sträng matats in under argumentet 'vektor' så måste strängen sparas om samt vektorn hämtas.
+    # Spara vektorns namn.
+    vektorNamn <- vektor
+    
+    # Hämta vektorn från det globala miljön.
+    vektor <- get(vektorNamn, envir = .GlobalEnv)
+  }
   
   # Kontrollera om vektorn är tom.
   if (length(vektor) == 0) {
