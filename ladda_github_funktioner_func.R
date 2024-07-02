a99e2356f549466c3e773ca04410b409e7269561
jl98swe <jl1998swe@gmail.com>
2024-07-02 12:27:23 +0200

    Initial commit.

---
+++ ladda_github_funktioner_func.R
@@ -0,0 +1,26 @@
+ladda_github_funktioner <- function(repo_owner, repo_name, branch = "main", folder = NULL, filtyp = ".R") {
+  
+  # Skapa URL:en.
+  url <- paste0("https://api.github.com/repos/", repo_owner, "/", repo_name, "/contents")
+  if (!is.null(folder)) {
+    url <- paste0(url, "/", folder)
+  }
+  
+  # Hämta innehållslistan.
+  contents <- gh::gh(
+    paste0("GET ", url),
+    owner = repo_owner,
+    repo = repo_name,
+    ref = branch
+  )
+  
+  # Ladda varje fil med en viss ändelse.
+  for (file in contents) {
+    if (endsWith(file$name, filtyp)) {
+      source_url(file$download_url)
+      cat("Loaded:", file$name, "\n")
+    }
+  }
+}
+
+# Exempel: ladda_github_funktioner(repo_owner = "jl98swe", repo_name = "jl98swe_funktioner")
